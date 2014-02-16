06ada6a622552cc870b4f5e4e644cc5eba5a3401
Kevin Ross <kevin.ross@alienfast.com>
2014-02-16 17:38:04 -0600

    Manual testing of hosted sessions works.

--- onlyroad-test.js
+++ onlyroad-test.js
@@ -6,9 +6,9 @@
  document.getElementsByTagName("head")[0].appendChild(e);
  */
 var load,execute,loadAndExecute,executeJQuery;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})}
-,executeJQuery=function(a){if(typeof jQuery=='undefined'){var jqUrl='//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js';loadAndExecute(jqUrl,a);}else{execute(a);}};
+    ,executeJQuery=function(a){if(typeof jQuery=='undefined'){var jqUrl='//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js';loadAndExecute(jqUrl,a);}else{execute(a);}};
 
-executeJQuery(function(){
+executeJQuery(function () {
 
     // On membersite, make 'series' link always go to the road filter
     $("td.simpleNav ul li a[href='/membersite/member/Series.do']").attr("href", "/membersite/member/Series.do?cat=2")
@@ -62,84 +62,90 @@ executeJQuery(function(){
     //  Filter the hosted sessions by road tracks.
     //      See the filter_tracks.js to generate the array.
     //
-    if(window.location.pathname.indexOf("/HostedSessions.do") > 0){
+    if (window.location.pathname.indexOf("/HostedSessions.do") > 0) {
 
-        // Generated output
-        var ovalTracks = [
-            52, // Oval - Atlanta Motor Speedway - Legends Oval
-            53, // Oval - Atlanta Motor Speedway - Oval
-            225, // Oval - Auto Club Speedway - Oval
-            101, // Oval - Bristol Motor Speedway -
-            143, // Oval - Centripetal Circuit -
-            39, // Oval - Charlotte Motor Speedway - Legends Oval
-            40, // Oval - Charlotte Motor Speedway - Oval
-            123, // Oval - Chicagoland Speedway -
-            15, // Oval - Concord Speedway -
-            115, // Oval - Darlington Raceway -
-            191, // Oval - Daytona International Speedway - Oval
-            27, // Oval - Daytona circa 2007 - Oval
-            162, // Oval - Dover International Speedway -
-            20, // Oval - Homestead Miami Speedway - Miami Speedway
-            133, // Oval - Indianapolis Motor Speedway - Oval
-            178, // Oval - Indianapolis Motor Speedway - IndyCar Oval
-            169, // Oval - Iowa Speedway - Oval
-            171, // Oval - Iowa Speedway - Legends
-            172, // Oval - Iowa Speedway - Infield Legends
-            19, // Oval - Irwindale Speedway - Inner
-            23, // Oval - Irwindale Speedway - Outer
-            30, // Oval - Irwindale Speedway - Outer - Inner
-            214, // Oval - Kansas Speedway - Oval
-            189, // Oval - Kentucky Speedway - Legends
-            188, // Oval - Kentucky Speedway - Oval
-            201, // Oval - Langley Speedway -
-            17, // Oval - Lanier National Speedway -
-            113, // Oval - Las Vegas Motor Speedway - Infield Legends Oval
-            103, // Oval - Las Vegas Motor Speedway - Oval
-            110, // Oval - Las Vegas Motor Speedway - Legends Oval
-            33, // Oval - Martinsville Speedway -
-            124, // Oval - Michigan International Speedway -
-            156, // Oval - Mid-Ohio Sports Car Course - Oval
-            157, // Oval - Mid-Ohio Sports Car Course - Alt Oval
-            131, // Oval - New Hampshire Motor Speedway - Oval
-            222, // Oval - New Hampshire Motor Speedway - Legends
-            190, // Oval - New Smyrna Speedway -
-            12, // Oval - Oxford Plains Speedway -
-            104, // Oval - Phoenix International Raceway - Oval
-            136, // Oval - Pocono Raceway - Oval
-            31, // Oval - Richmond International Raceway -
-            203, // Oval - Rockingham Speedway - Oval
-            14, // Oval - South Boston Speedway -
-            11, // Oval - Stafford Motor Speedway - Full Course
-            116, // Oval - Talladega Superspeedway -
-            120, // Oval - Texas Motor Speedway - Legends Oval
-            121, // Oval - Texas Motor Speedway - Oval
-            94, // Oval - The Milwaukee Mile -
-            161, // Oval - Thompson International Speedway -
-            198, // Oval - Twin Ring Motegi - Oval
-            16 // Oval - USA International Speedway -
-        ];
+        // Change something to indicate this is filtered.
+        $("tr th a[name='Track']").text("Track (road only)");
 
-        function getURLParameter(url, name) {
-            return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(url)||[,""])[1].replace(/\+/g, '%20'))||null;
-        }
 
-        // Iterate over each td that contains a link to a track, compare to known roadTracks and remove if not listed above.
-        // /membersite/member/TrackDetail.do?trkid=107
-        $("table tr td div div a[href*='/membersite/member/TrackDetail.do?trkid']").each(function( index ) {
-            var href = $( this ).attr('href');
-            var trackID = parseInt(getURLParameter(href, 'trkid'));
-            var text = $(this).text();
-            var isOval = (-1 != $.inArray(trackID, ovalTracks));
-            var isUnwantedRow = (text == 'Track Info' || text == '')
-            //console.log("track["+ trackID + "]: " + isOval + " - " + $(this).text())
+//        setInterval(function () {
+            // Generated output
+            var ovalTracks = [
+                52, // Oval - Atlanta Motor Speedway - Legends Oval
+                53, // Oval - Atlanta Motor Speedway - Oval
+                225, // Oval - Auto Club Speedway - Oval
+                101, // Oval - Bristol Motor Speedway -
+                143, // Oval - Centripetal Circuit -
+                39, // Oval - Charlotte Motor Speedway - Legends Oval
+                40, // Oval - Charlotte Motor Speedway - Oval
+                123, // Oval - Chicagoland Speedway -
+                15, // Oval - Concord Speedway -
+                115, // Oval - Darlington Raceway -
+                191, // Oval - Daytona International Speedway - Oval
+                27, // Oval - Daytona circa 2007 - Oval
+                162, // Oval - Dover International Speedway -
+                20, // Oval - Homestead Miami Speedway - Miami Speedway
+                133, // Oval - Indianapolis Motor Speedway - Oval
+                178, // Oval - Indianapolis Motor Speedway - IndyCar Oval
+                169, // Oval - Iowa Speedway - Oval
+                171, // Oval - Iowa Speedway - Legends
+                172, // Oval - Iowa Speedway - Infield Legends
+                19, // Oval - Irwindale Speedway - Inner
+                23, // Oval - Irwindale Speedway - Outer
+                30, // Oval - Irwindale Speedway - Outer - Inner
+                214, // Oval - Kansas Speedway - Oval
+                189, // Oval - Kentucky Speedway - Legends
+                188, // Oval - Kentucky Speedway - Oval
+                201, // Oval - Langley Speedway -
+                17, // Oval - Lanier National Speedway -
+                113, // Oval - Las Vegas Motor Speedway - Infield Legends Oval
+                103, // Oval - Las Vegas Motor Speedway - Oval
+                110, // Oval - Las Vegas Motor Speedway - Legends Oval
+                33, // Oval - Martinsville Speedway -
+                124, // Oval - Michigan International Speedway -
+                156, // Oval - Mid-Ohio Sports Car Course - Oval
+                157, // Oval - Mid-Ohio Sports Car Course - Alt Oval
+                131, // Oval - New Hampshire Motor Speedway - Oval
+                222, // Oval - New Hampshire Motor Speedway - Legends
+                190, // Oval - New Smyrna Speedway -
+                12, // Oval - Oxford Plains Speedway -
+                104, // Oval - Phoenix International Raceway - Oval
+                136, // Oval - Pocono Raceway - Oval
+                31, // Oval - Richmond International Raceway -
+                203, // Oval - Rockingham Speedway - Oval
+                14, // Oval - South Boston Speedway -
+                11, // Oval - Stafford Motor Speedway - Full Course
+                116, // Oval - Talladega Superspeedway -
+                120, // Oval - Texas Motor Speedway - Legends Oval
+                121, // Oval - Texas Motor Speedway - Oval
+                94, // Oval - The Milwaukee Mile -
+                161, // Oval - Thompson International Speedway -
+                198, // Oval - Twin Ring Motegi - Oval
+                16 // Oval - USA International Speedway -
+            ];
 
-            if(isOval && !isUnwantedRow){
-                tr = $(this).parent().parent().parent().parent();
-                //console.log(tr);
-                console.log("Removing track["+ trackID + "]: " + isOval + " - " + text)
-                tr.toggle();
+            function getURLParameter(url, name) {
+                return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(url) || [, ""])[1].replace(/\+/g, '%20')) || null;
             }
-        });
+
+            // Iterate over each td that contains a link to a track, compare to known roadTracks and remove if not listed above.
+            // /membersite/member/TrackDetail.do?trkid=107
+            $("table tr td div div a[href*='/membersite/member/TrackDetail.do?trkid']").each(function (index) {
+                var href = $(this).attr('href');
+                var trackID = parseInt(getURLParameter(href, 'trkid'));
+                var text = $(this).text();
+                var isOval = (-1 != $.inArray(trackID, ovalTracks));
+                var isUnwantedRow = (text == 'Track Info' || text == '')
+                //console.log("track["+ trackID + "]: " + isOval + " - " + $(this).text())
+
+                if (isOval && !isUnwantedRow) {
+                    tr = $(this).parent().parent().parent().parent();
+                    //console.log(tr);
+                    console.log("Removing track[" + trackID + "]: " + isOval + " - " + text)
+                    tr.toggle();
+                }
+            });
+//        }, 6000);
     }
 
 
@@ -147,22 +153,23 @@ executeJQuery(function(){
     //
     //  Only execute the following if we are on the forum list page
     //
-    if(window.location.pathname.indexOf("/list.page") <= 0)
+    if (window.location.pathname.indexOf("/list.page") <= 0)
         return;
     else
         console.log("Skipping forum list pruning.");
 
-    function toggleForumRowByText(forums){
+    function toggleForumRowByText(forums) {
 
-        $.each(forums, function( index, value ) {
+        $.each(forums, function (index, value) {
             e = $("td:contains('" + value + "')");
             e.parent().toggle();
             console.log("Removed: " + value);
         });
     }
-    function toggleForumRowByPage(forums){
 
-        $.each(forums, function( index, value ) {
+    function toggleForumRowByPage(forums) {
+
+        $.each(forums, function (index, value) {
             e = $("td a[href*='/" + value + ".page']");
             text = e.text();
             e.parent().parent().toggle();
