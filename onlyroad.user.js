40fc8b1364c8e384df876ded8812b30e1559d272
Kevin Ross <kevin.ross@alienfast.com>
2014-04-24 20:00:25 -0500

    version bump - add back paint section

--- onlyroad.user.js
+++ onlyroad.user.js
@@ -4,7 +4,7 @@
 // @description Userscript for iRacing that hides content unrelated to road racing.  This is opinionated, so YMMV.
 // @include     http://members.iracing.com/jforum/forums/list.page
 // @include     http://members.iracing.com/membersite/member/*
-// @version     5
+// @version     6
 // @grant       none
 // ==/UserScript==
 var load,execute,loadAndExecute,executeJQuery;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})}
